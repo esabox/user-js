e5d9fe586fe61d036600deb438c205bb47a950ea
esabox <esabox@live.jp>
2022-11-14 16:52:18 +0900

    Update local_file_reuire.user.js

--- local_file_reuire.user.js
+++ local_file_reuire.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         local_load@codepen
+// @name           @req file:///userjs
+// @description fileプロトコルで@require
 // @namespace    https://gist.github.com/esabox/
-// @updateURL    https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
-// @homepageURL  https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
-// @version 2019.12.02.171237
+// @updateURL     https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
+// @downloadURL https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
+// @version      2022-11-14
 // @author       nank
 // @match        *://*/*
 // @exclude      ^https://codepen.io/*
-// @exclude      ^http://localhost*
-// x@exclude    /^http://localhost.*/
 // @exclude     https://docs.google.com/*
 // @grant	GM_registerMenuCommand
 // @grant 	GM_getValue
@@ -17,30 +16,21 @@
 // @grant 	GM_listValues
 // @grant   GM_xmlhttpRequest
 // @noframes
-// @require       http://localhost:8888/lib/FileSaver.js
-// @require       http://localhost:8888/lib/jszip.js
 // @require       file:///Users/kazoku/Desktop/code/mypo.user.js
-// x@require      http://localhost:8888/mypo.user.js
 // ==/UserScript==
 
-// @updateURL    https://codepen.io/esabox/pen/bzxpzo.js
-// @homepageURL  https://codepen.io/esabox/pen/bzxpzo?editors=0012
-// @include *
-// @homepageURL  https://gist.github.com/esabox/e168594e77f35c6e18e4b5ecbe4272c0/edit
-// @downloadURL  https://gist.github.com/esabox/e168594e77f35c6e18e4b5ecbe4272c0/raw/mbpR13Late.user.js
-// @connect      localhost:8888
-/*
-let zip = new JSZip();
-console.log(zip)
-console.log("tag load")
-
-let s = document.createElement( 'script' );
-s.type = 'text/javascript';
-s.src = ( 'http://localhost:8888/mypo.user.js'); //+"?"+Date.now() //引数あるとvscodeで止まらない、logの行数だけは移動できた。
-document.body.appendChild( s );
-
-gistにあげようとしたら、二段階認証がめんどくてcodepenに
-
-2019/11/18
-githubに変更
-*/
+// x@require      http://localhost:8888/mypo.user.js
+// if(globalThis.endFlag )return;
+// import('http://localhost:8888/mypo.user.js')
+// 		.then(() => {
+// 			console.log(111)
+// 		})
+//require ローカルで、無ければネット
+// if(window.end)return ;
+// !function(){
+//     const main = function() {
+//         alert('main')
+//     }
+//     main()
+// }()
+// window.end=1
